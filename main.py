a4f597d31a52486645ddba0a94f38a42812ff665
Arseny Amerbaev <amerbaev@gmail.com>
2017-12-05 19:02:41 +0300

    Распараллеленная обработка моделей

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn import datasets
 
+import concurrent.futures
+
 
 def region_query(P, eps, D):
     neighbour_pts = {}
@@ -75,61 +77,89 @@ def dbscan(X, eps, min_pts):
 
 if __name__ == '__main__':
     n_samples = 500
+    random_state = 170
+    transformation = [[0.6, -0.6], [-0.4, 0.8]]
 
-    blobs = datasets.make_blobs(n_samples=n_samples, random_state=8, center_box=(-2000, 2000), cluster_std=50)
-    dbscan(blobs[0], 100, 10)
+    models = [
+        {
+            'X': datasets.make_blobs(n_samples=n_samples, random_state=8, center_box=(-2000, 2000), cluster_std=50)[0],
+            'eps': 100,
+            'min_pts': 10,
+        },
+        {
+            'X': datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)[0],
+            'eps': 0.2,
+            'min_pts': 6,
+        },
+        {
+            'X': datasets.make_moons(n_samples=n_samples, noise=.05)[0],
+            'eps': 0.2,
+            'min_pts': 6,
+        },
+        {
+            'X': datasets.make_blobs(n_samples=n_samples, random_state=8)[0],
+            'eps': 0.5,
+            'min_pts': 15,
+        },
+        {
+            'X': np.random.rand(n_samples, 2),
+            'eps': 0.3,
+            'min_pts': 6,
+        },
+        {
+            # Anisotropicly distributed data
+            'X': np.dot(datasets.make_blobs(n_samples=n_samples, random_state=random_state)[0], transformation),
+            'eps': 0.4,
+            'min_pts': 6,
+        },
+        {
+            # blobs with varied variances
+            'X': datasets.make_blobs(n_samples=n_samples,
+                                     cluster_std=[1.0, 2.5, 0.5],
+                                     random_state=random_state)[0],
+            'eps': 0.3,
+            'min_pts': 12,
+        },
+        {
+            'X': datasets.make_blobs(n_samples=n_samples, n_features=2, centers=5)[0],
+            'eps': 0.9,
+            'min_pts': 15,
+        },
+        {
+            'X': datasets.make_gaussian_quantiles(n_samples=n_samples, n_features=2, n_classes=6)[0],
+            'eps': 0.9,
+            'min_pts': 8,
+        },
+        {
+            'X': datasets.make_circles(n_samples=n_samples, factor=0.5)[0],
+            'eps': 0.25,
+            'min_pts': 5,
+        },
+        {
+            'X': datasets.make_classification(n_samples=n_samples, n_features=2, n_redundant=0, n_informative=1,
+                                              n_clusters_per_class=1)[0],
+            'eps': 0.3,
+            'min_pts': 8,
+        },
+        {
+            'X': datasets.make_classification(n_samples=n_samples, n_features=2, n_redundant=0, n_informative=2,
+                                              n_clusters_per_class=1)[0],
+            'eps': 0.3,
+            'min_pts': 12,
+        },
+        {
+            'X': datasets.make_classification(n_samples=n_samples, n_features=2, n_redundant=0, n_informative=2)[0],
+            'eps': 0.5,
+            'min_pts': 8,
+        },
+        {
+            'X': datasets.make_classification(n_samples=n_samples, n_features=2, n_redundant=0, n_informative=2,
+                                              n_clusters_per_class=1, n_classes=3)[0],
+            'eps': 0.1,
+            'min_pts': 6,
+        },
+    ]
 
-    # noisy_circles = datasets.make_circles(n_samples=n_samples, factor=.5,
-    #                                       noise=.05)
-    #
-    # dbscan(noisy_circles[0], 0.2, 6)
-    #
-    # noisy_moons = datasets.make_moons(n_samples=n_samples, noise=.05)
-    #
-    # dbscan(noisy_moons[0], 0.2, 6)
-    #
-    # blobs = datasets.make_blobs(n_samples=n_samples, random_state=8)
-    # dbscan(blobs[0], 0.5, 15)
-    #
-    # no_structure = np.random.rand(n_samples, 2), None
-    # dbscan(no_structure[0], 0.3, 6)
-    #
-    # # Anisotropicly distributed data
-    # random_state = 170
-    # X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
-    # transformation = [[0.6, -0.6], [-0.4, 0.8]]
-    # X_aniso = np.dot(X, transformation)
-    # aniso = (X_aniso, y)
-    #
-    # dbscan(X_aniso, 0.4, 6)
-    #
-    # # blobs with varied variances
-    # varied = datasets.make_blobs(n_samples=n_samples,
-    #                              cluster_std=[1.0, 2.5, 0.5],
-    #                              random_state=random_state)
-    # dbscan(varied[0], 0.3, 12)
-
-    #
-    # X, Y = datasets.make_blobs(n_samples=n_samples, n_features=2, centers=5)
-    # dbscan(X, 0.9, 15)
-    #
-    # X, Y = datasets.make_gaussian_quantiles(n_samples=n_samples, n_features=2, n_classes=6)
-    # dbscan(X, 0.9, 8)
-    #
-    # X, Y = datasets.make_circles(n_samples=n_samples, factor=0.5)
-    # dbscan(X, 0.25, 5)
-    #
-    # X, Y = datasets.make_classification(n_samples=n_samples, n_features=2, n_redundant=0, n_informative=1,
-    #                                     n_clusters_per_class=1)
-    # dbscan(X, 0.3, 8)
-    #
-    # X, Y = datasets.make_classification(n_samples=n_samples, n_features=2, n_redundant=0, n_informative=2,
-    #                                     n_clusters_per_class=1)
-    # dbscan(X, 0.3, 12)
-    #
-    # X, Y = datasets.make_classification(n_samples=n_samples, n_features=2, n_redundant=0, n_informative=2)
-    # dbscan(X, 0.5, 8)
-    #
-    # X, Y = datasets.make_classification(n_samples=n_samples, n_features=2, n_redundant=0, n_informative=2,
-    #                                     n_clusters_per_class=1, n_classes=3)
-    # dbscan(X, 0.2, 6)
+    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as e:
+        for m in models:
+            e.submit(dbscan, **m)
